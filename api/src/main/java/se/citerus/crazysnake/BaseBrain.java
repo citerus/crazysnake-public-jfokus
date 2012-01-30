05daa05c17fde8585704bd5d44969e53a5d9300c
Anton Gravestam <anton.gravestam@gmail.com>
2012-01-30 16:27:20 +0100

    Added two helper sot AbstractBrain

--- api/src/main/java/se/citerus/crazysnake/BaseBrain.java
+++ api/src/main/java/se/citerus/crazysnake/BaseBrain.java
@@ -14,6 +14,38 @@ public abstract class BaseBrain implements Brain {
 
     protected HeatMeta meta;
     protected Set<BrainId> participants;
+    
+    /**
+     * 
+     * @param state, the current heat state.
+     * @return Returns your opponent in the heat as a Snake.
+     */
+    protected Snake getOpponent(HeatState state) {
+    	BrainId brainId = new BrainId(this);
+    	for (BrainId id : participants) {
+			if(!id.equals(brainId)) {
+				return state.getSnake(id);
+			}
+		}
+    	return null;
+    }
+    
+    /**
+     * 
+     * @param state, the current heat state.
+     * @return Returns your own brains Snake.
+     */
+    protected Snake getMe(HeatState state) {
+    	BrainId brainId = new BrainId(this);
+    	for (BrainId id : participants) {
+			if(id.equals(brainId)) {
+				return state.getSnake(id);
+			}
+		}
+    	return null;
+    }
+    
+    
 
     /**
      * Default, empty implementation.
